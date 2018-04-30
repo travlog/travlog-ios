ca90a5cc2f947b1ac95e2a448132c2b640185e00
devjin0617 <devjin0617@gmail.com>
2018-05-01 01:52:54 +0900

    프로젝트 초기화

---
+++ travlog-ios/travlog-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travlog-ios
+//
+//  Created by jin on 2018. 5. 1..
+//  Copyright © 2018년 travlog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ travlog-ios/travlog-ios/util/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  travlog-ios
+//
+//  Created by jin on 2018. 5. 1..
+//  Copyright © 2018년 travlog. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Service {
+    class func request (type: String, url: String) {
+    }
+}
